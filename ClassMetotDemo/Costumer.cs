1fd2c4c31cd3d6c94461741ccfc0f134d3f56167
Murat UTLU <45118440+jinakuma@users.noreply.github.com>
2021-01-20 01:11:48 +0300

    Add project files.

---
+++ ClassMetotDemo/Costumer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Costumer
+    {   
+        public int CostumerId { get; set; }
+        public string CostumerName { get; set; }
+        public string CostumerSurname { get; set; }
+        public string CostumerAdress { get; set; }
+    }
+}
---
+++ ReferansValue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReferansValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine(add(a,b));
+            Console.WriteLine(a);
+            Console.WriteLine(add2(ref a, b));
+            Console.WriteLine(a);
+            static int  add(int a, int b)
+            {
+                a = 20;
+                return a + b;
+            }
+
+            static int add2(ref int a, int b)
+            {
+                a = 20;
+                return a + b;
+            }
+        }
+    }
+}
