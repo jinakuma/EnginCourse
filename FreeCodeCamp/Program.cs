61387350bfb0b28db57ab34660433070a244b206
Murat UTLU <45118440+jinakuma@users.noreply.github.com>
2021-01-24 16:28:07 +0300

    Revert "Delete FreeCodeCamp directory"
    This reverts commit 1cf0361b0667df2800c9adea3fc7300c72dc30a5.

---
+++ FreeCodeCamp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FreeCodeCamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
