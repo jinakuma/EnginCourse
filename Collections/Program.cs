d7767defa6ea9f9b9d72620237a867a8fc096b63
Murat UTLU <45118440+jinakuma@users.noreply.github.com>
2021-01-24 15:57:04 +0300

    reset

---
+++ Collections/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> {};
+            names.Add("Murat");
+            Console.WriteLine(names[0]);
+        }
+    }
+}
